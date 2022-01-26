045989e7ded01c368d3612cb1212a01a882946d3
samamoazemi <moazemisama@gmail.com>
2022-01-26 03:29:39 -0800

    Create store in redux

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+const redux = require("redux");
+const createStore = redux.createStore;
+
 // action :
 
 const BUY_CAKE = "BUY_CAKE";
@@ -20,9 +24,23 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch(action.type){
     case BUY_CAKE:
-      return { ...state, numOfCake: numOfCake - 1 }
+      return { ...state, numOfCake: state.numOfCake - 1 }
     default:
       return state;
   }
-
 }
+
+// store :
+
+const store = createStore(reducer);
+
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
