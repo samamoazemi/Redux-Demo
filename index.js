b1f30830db9c513dc3f00ed7b876efac345fe72c
samamoazemi <moazemisama@gmail.com>
2022-01-26 02:41:54 -0800

    three core concepts in redux

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// console.log("redux demo");
+
+// 3 core concepts in redux :
+
+/* 1. action : 
+ actions are a plain JavaScript object that contains information. */
+
+/* 2. reducer : 
+ reducers are the pure functions that take the current state and
+ action and return the new state and tell the store how to do. */
+
+/* 3. store : 
+ The store is the object which holds the state of the application. */
