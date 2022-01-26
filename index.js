758bd73fcbe5531f552e3c0bdc4e9a00f88f5393
samamoazemi <moazemisama@gmail.com>
2022-01-26 02:54:50 -0800

    Create action in redux

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
+// action :
 
-// console.log("redux demo");
+const BUY_CAKE = "BUY_CAKE";
 
-// 3 core concepts in redux :
-
-/* 1. action : 
- actions are a plain JavaScript object that contains information. */
-
-/* 2. reducer : 
- reducers are the pure functions that take the current state and
- action and return the new state and tell the store how to do. */
-
-/* 3. store : 
- The store is the object which holds the state of the application. */
+function buyCake() {  // action creator
+  return(
+    {
+      type: BUY_CAKE,
+    }
+  )
+}
